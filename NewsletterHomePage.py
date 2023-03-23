fd0f0f01f8ba5ce41c7dfaeb88c0ab20a59ccc14
Paulo Maia <47892819+MaiaPaulo@users.noreply.github.com>
2023-03-23 13:46:49 -0400

    First commit

--- NewsletterHomePage.py
+++ NewsletterHomePage.py
@@ -1,6 +1,16 @@
 # Importing Streamlit
 import pandas as pd
 import streamlit as st
+import pyodbc
+
+# Creating a new connection to a LOCAL DB
+conn_str = ("Driver={SQL Server};"
+            "Server=MAIALAPTOP;"
+            "Database=newsletter;"
+            "Trusted_Connection=yes;"
+            "Uid=auth_window")
+conn = pyodbc.connect(conn_str)
+cursor = conn.cursor()
 
 # Tittle and sub tittle of the web form
 st.set_page_config(layout="centered", page_icon="📖", page_title="Newsletter registration")
@@ -8,16 +18,24 @@ st.title("📖 Newsletter registration")
 st.subheader("The main informations about Tech twice a week!")
 
 # Parameters for tto get the information
-with st.form("Register", clear_on_submit=True):
-    name = st.text_input("Enter your full name")
+with st.form(key="Register", clear_on_submit=True):
+    firstname = st.text_input("Enter your first name")
+    lastname = st.text_input("Enter your last name")
     college = st.text_input("Where do you study?")
     course = st.text_input("What is your field of study?")
     email = st.text_input("What is your best email?")
-    submit = st.form_submit_button("I want to get connected")
+    submit_register = st.form_submit_button("I want to get connected")
 
-    if submit:
-        print(name)
+# Creating lists and dictionaries to insert the new data
+    if submit_register:
+        dfUsers = pd.DataFrame({"Name": [firstname], "Last Name": [lastname],
+                                "College": [college], "Course": [course], "Email": [email]})
+        st.success("Your registration is complete {}".format(firstname))
+        st.balloons()
+        # Execute the query
+        cursor.execute("INSERT INTO Register (FirstName, LastName, College, Course, Email) VALUES (?, ?, ?, ?, ? )",
+                       (firstname, lastname, college, course, email))
 
-# To run the code -> streamlit run NewsletterHomePage.py
-print(email)
-
+        # the connection is not autocommited by default. So we must commit to save our changes.
+        conn.commit()
+        print(dfUsers)
